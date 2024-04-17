1ecfd1aa78f8826b48ed3bd5e30c62df1f2ac58a
Mieko Hikari <77004524+MiekoHikari@users.noreply.github.com>
2024-04-17 21:55:17 +0400

    Loaded Starter Template

---
+++ src/index.ts
@@ -0,0 +1,40 @@
+import { ApplicationCommandRegistries, RegisterBehavior } from '@sapphire/framework';
+import '@sapphire/plugin-logger/register';
+import * as colorette from 'colorette';
+import dotenv from 'dotenv';
+import { LogLevel, SapphireClient } from '@sapphire/framework';
+import { GatewayIntentBits } from 'discord.js';
+
+// Set default behavior to bulk overwrite
+ApplicationCommandRegistries.setDefaultBehaviorWhenNotIdentical(RegisterBehavior.BulkOverwrite);
+
+// Load .env file
+dotenv.config();
+
+// Enable colorette
+colorette.createColors({ useColor: true });
+
+// Initialize the client
+const client = new SapphireClient({
+	defaultPrefix: '!',
+	caseInsensitiveCommands: true,
+	logger: {
+		level: LogLevel.Debug
+	},
+	intents: [GatewayIntentBits.DirectMessages, GatewayIntentBits.GuildMessages, GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent],
+	loadMessageCommandListeners: true
+});
+
+const main = async () => {
+	try {
+		client.logger.info('Logging in');
+		await client.login();
+		client.logger.info('logged in');
+	} catch (error) {
+		client.logger.fatal(error);
+		await client.destroy();
+		process.exit(1);
+	}
+};
+
+void main();
