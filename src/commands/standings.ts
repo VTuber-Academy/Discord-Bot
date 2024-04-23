e43304b9f75f0ffd73baf640573dca016a1a753a
Mieko Hikari <77004524+MiekoHikari@users.noreply.github.com>
2024-04-23 19:46:25 +0400

    Add Standings Command

---
+++ src/commands/standings.ts
@@ -0,0 +1,25 @@
+import { ApplyOptions } from '@sapphire/decorators';
+import { Command } from '@sapphire/framework';
+import strikeManager from '../lib/managers/strikeManager';
+
+@ApplyOptions<Command.Options>({
+	description: "See a member's moral standings"
+})
+export class UserCommand extends Command {
+	public override registerApplicationCommands(registry: Command.Registry) {
+		registry.registerChatInputCommand((builder) =>
+			builder //
+				.setName(this.name)
+				.setDescription(this.description)
+				.addUserOption((option) => option.setName('target').setDescription('Member to see the standings of'))
+		);
+	}
+
+	public override async chatInputRun(interaction: Command.ChatInputCommandInteraction) {
+		const user = interaction.options.getUser('target') ?? interaction.user;
+
+		const standings = await strikeManager.generateStandings(user);
+
+		return interaction.reply({ embeds: [standings], ephemeral: true });
+	}
+}
