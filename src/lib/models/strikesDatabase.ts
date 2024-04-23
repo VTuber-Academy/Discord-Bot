dcd5e8314d855b33050e4865070d64aba62d9289
Mieko Hikari <77004524+MiekoHikari@users.noreply.github.com>
2024-04-23 20:47:01 +0400

    modify the database model

--- src/lib/models/strikesDatabase.ts
+++ src/lib/models/strikesDatabase.ts
@@ -1,4 +1,4 @@
-import mongoose, { Schema } from 'mongoose';
+import mongoose, { Schema, Document } from 'mongoose';
 
 // Define the interface for your document
 export interface IStrike {
@@ -8,18 +8,26 @@ export interface IStrike {
 	strikeId: string;
 }
 
-export interface IUser {
+export interface IUser extends Document {
 	userId: string;
 	strikes: IStrike[];
 }
 
-// Define the schema for your document
+// Define the schema for your Strike
 const StrikeSchema: Schema = new Schema({
+	reason: { type: String, required: true },
+	timestamp: { type: Date, required: true },
+	moderatorId: { type: String, required: true },
+	strikeId: { type: String, required: true }
+});
+
+// Define the schema for your User
+const UserSchema: Schema = new Schema({
 	userId: { type: String, required: true },
-	strikes: [{ type: Schema.Types.ObjectId, ref: 'Strike', default: [] }]
+	strikes: { type: [StrikeSchema], default: [] }
 });
 
 // Define the model for your document
-const StrikeModel = mongoose.model<IUser>('Strikes', StrikeSchema);
+const UserModel = mongoose.model<IUser>('User', UserSchema);
 
-export default StrikeModel;
+export default UserModel;
